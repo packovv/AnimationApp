b8c92f20dffaf5f89a416f5844e5c5acd72f48c1
Pavel Kachan <KachanPG@yandex.ru>
2022-01-12 17:01:34 +0300

    Changed UI colours, added cornerRadius

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationView.layer.cornerRadius = 15
+        runAnimationButton.layer.cornerRadius = 15
     }
 
     @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
